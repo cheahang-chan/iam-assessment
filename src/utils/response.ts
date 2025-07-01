844e99e9bc60b8b061d5d99b89f08b0e5b038498
Chea Hang <cheahang@outlook.com>
2025-07-01 12:58:31 +0800

    chore: update message json in response util

--- src/utils/response.ts
+++ src/utils/response.ts
@@ -1,12 +1,12 @@
 import { Response } from 'express';
 import { HttpStatus } from './http-status';
 
-export function error(res: Response, msg: string, data: any = {}, status = HttpStatus.INTERNAL_SERVER_ERROR) {
+export function error(res: Response, message: string, data: any = {}, status = HttpStatus.INTERNAL_SERVER_ERROR) {
     res.status(status);
-    return res.json({ status: false, msg, data });
+    return res.json({ status: false, message, data });
 }
 
-export function success(res: Response, msg: string, data: any = {}, status = HttpStatus.OK) {
+export function success(res: Response, message: string, data: any = {}, status = HttpStatus.OK) {
     res.status(status);
-    return res.json({ status: true, msg, data });
+    return res.json({ status: true, message, data });
 }
